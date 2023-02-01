722ff4c1d684bb2063cc6fab7fde3520ed777e86
omitea8 <omitea8@gmail.com>
2023-02-01 11:53:14 +0900

    twitterからのcallbackURLからstateとcodeを取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { redirectToAuth } from "./buttonActions";
+import { GetState } from "./getState";
 
 //App
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <div>
+          <p>caita</p>
+          <button onClick={redirectToAuth}>login</button>
+        </div>
+      ),
+    },
+    {
+      path: "/auth/twitter/callback",
+      element: <GetState />,
+    },
+  ]);
   return (
     <div>
-      <p>caita</p>
-
-      <button onClick={redirectToAuth}>login</button>
+      <RouterProvider router={router} />
     </div>
   );
 }
