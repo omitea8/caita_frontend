e4f80c6a0c271c2c0bc790bd92a79c991f9b1522
omitea8 <omitea8@gmail.com>
2023-02-08 11:39:24 +0900

    トーストを追加

--- src/getState.tsx
+++ src/getState.tsx
@@ -1,10 +1,12 @@
 import { useEffect } from "react";
 import { useSearchParams } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 
 export const GetState = () => {
   const [searchParams, setSearchParams] = useSearchParams();
 
   useEffect(() => {
+    // stateとcodeをバックエンドに送る
     const sendData = {
       state: searchParams.get("state"),
       code: searchParams.get("code"),
@@ -13,11 +15,23 @@ export const GetState = () => {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(sendData),
-    });
+    })
+      // トークンリクエストの結果を取得する
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message === "OK") {
+          toast.success("login OK");
+          console.log(data.message);
+        } else {
+          toast.error("login failed");
+          console.log(data.message);
+        }
+      });
   }, [searchParams]);
   return (
     <>
       <p>caita redirect</p>
+      <Toaster position="top-center" reverseOrder={false} />
     </>
   );
 };
