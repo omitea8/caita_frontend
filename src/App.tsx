bd2f99b2fa3c9f019d17c3ee2fc2c3b42254853e
omitea8 <omitea8@gmail.com>
2023-02-08 16:21:54 +0900

    ログインのコンポーネントを切り出し、buttonActionsを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { redirectToAuth } from "./buttonActions";
 import { GetState } from "./GetState";
+import { Login } from "./Login";
 
 //App
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: (
-        <div>
-          <p>caita</p>
-          <button onClick={redirectToAuth}>login</button>
-        </div>
-      ),
+      element: <Login />,
     },
     {
       path: "/auth/twitter/callback",
