b153cb97177dac6ec11007e92ee5c08a59fda5be
omitea8 <omitea8@gmail.com>
2023-03-23 14:49:14 +0900

    TypeScriptのeslintの設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,8 +3,12 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:@typescript-eslint/strict",
   ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
 };
