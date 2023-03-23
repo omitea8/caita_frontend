8663a4a454f28bec41ecb31d74a4c4b6a444dc3c
omitea8 <omitea8@gmail.com>
2023-03-23 14:50:44 +0900

    画像の投稿機能の追加・投稿成功時にリダイレクトをmypageに設定

--- src/PostPage.tsx
+++ src/PostPage.tsx
@@ -1,37 +1,62 @@
 import { FC, useState } from "react";
 import toast, { Toaster } from "react-hot-toast";
 import { TextField } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 export const PostPage: FC = () => {
   const [captionText, setCaptionText] = useState("");
+  const [postImage, setPostImage] = useState<File | null>(null);
+  const navigate = useNavigate();
 
   const Post = () => {
-    const data = { caption: captionText };
+    const data = new FormData();
+    data.append("caption", captionText);
+    console.log(postImage);
+    if (postImage) {
+      data.append("image", postImage);
+    }
     fetch("/images/post", {
-      headers: {
-        "Content-Type": "application/json",
-      },
       method: "POST",
-      body: JSON.stringify(data),
+      body: data,
     })
       .then((response) => response.json())
       .then((data) => {
         if (data === "OK") {
           toast.success("投稿しました");
+          navigate("/MyPage");
         } else {
           toast.error("投稿に失敗しました");
         }
+      })
+      .catch((error) => {
+        toast.error("エラー");
       });
     setCaptionText("");
+    setPostImage(null);
   };
 
   const upCaptionText = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCaptionText(event.target.value);
   };
+  const upPostImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files[0]) {
+      console.log(event.target.files);
+      setPostImage(event.target.files[0]);
+    }
+  };
+
   return (
     <div>
       <p>caita</p>
-      <TextField label="caption" value={captionText} onChange={upCaptionText} />
+      <form>
+        <TextField type="text" value={captionText} onChange={upCaptionText} />
+        <TextField
+          type="file"
+          onChange={upPostImage}
+          InputLabelProps={{ shrink: true }}
+          InputProps={{ disableUnderline: true }}
+        />
+      </form>
       <p>
         <button onClick={Post}>投稿</button>
       </p>
