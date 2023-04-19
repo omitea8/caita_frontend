20ab414fd3838385f005bd677213547c280e6c4e
omitea8 <omitea8@gmail.com>
2023-04-19 22:37:41 +0900

    MenuBarのスタイルを変更

--- src/MenuBar.tsx
+++ src/MenuBar.tsx
@@ -2,7 +2,11 @@ import { AppBar, Avatar, Button, Toolbar, Typography } from "@mui/material";
 
 export const MenuBar: React.FC = () => {
   return (
-    <AppBar position="static" sx={{ bgcolor: "black", mb: 3 }} elevation={0}>
+    <AppBar
+      position="static"
+      sx={{ bgcolor: "black", mb: 3, maxHeight: 70 }}
+      elevation={0}
+    >
       <Toolbar>
         <Typography variant="h5" component="div" sx={{ flexGrow: 1 }}>
           Caita
