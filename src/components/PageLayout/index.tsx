386d1db08f45b132a0050ca00ef75c268a47f6f1
omitea8 <omitea8@gmail.com>
2023-06-27 20:02:11 +0900

    不要なimportの削除

--- src/components/PageLayout/index.tsx
+++ src/components/PageLayout/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 import { Footer } from "./Footer";
 import { MenuBar } from "./MenuBar";
 import { ReactNode } from "react";
