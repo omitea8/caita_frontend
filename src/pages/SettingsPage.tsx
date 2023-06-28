9026dd48d85412437835f2d4a09835cfacf23079
omitea8 <omitea8@gmail.com>
2023-06-28 18:04:41 +0900

    アカウントの削除機能の実装

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { PageLayout } from "../components/PageLayout";
 import { useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
-import { Button, Stack, Typography } from "@mui/material";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { DeleteForever } from "@mui/icons-material";
+import toast from "react-hot-toast";
 
 interface ProfileData {
   profile_image_url: string;
@@ -24,6 +34,40 @@ export const SettingsPage: React.FC = () => {
     ).then((res) => res.json());
   });
 
+  // Dialogの設定
+  const [open, setOpen] = React.useState(false);
+  const [clickedCreatorId, setClickedCreatorId] = React.useState("");
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // アカウントの削除
+  const deleteMutation = useMutation(
+    (creatorID: string) => {
+      return fetch(
+        `${process.env.REACT_APP_API_URL ?? ""}/creators/${creatorID}`,
+        {
+          method: "DELETE",
+          credentials: "include",
+        }
+      );
+    },
+    {
+      onSuccess: (response) => {
+        if (response.status === 200) {
+          toast.success("アカウントを削除しました。");
+          // imagesQuery.refetch().catch(console.error);
+          navigate("/");
+        } else {
+          toast.error("アカウントの削除に失敗しました。");
+        }
+      },
+    }
+  );
+
   return (
     <PageLayout>
       <Stack alignItems="center" spacing={1}>
@@ -38,14 +82,43 @@ export const SettingsPage: React.FC = () => {
         </Typography>
 
         <Button
+          variant="outlined"
+          color="error"
           startIcon={<DeleteForever />}
           onClick={() => {
-            navigate("/");
+            setClickedCreatorId(profileQuery.data?.username ?? "");
+            handleClickOpen();
           }}
-          color="error"
         >
           アカウントの削除
         </Button>
+        {/* Dialog */}
+        <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+        >
+          <DialogTitle id="alert-dialog-title">
+            アカウントを削除しますか？
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              アカウントの削除を行うと、アカウントは即時削除され投稿した画像も全て削除されます。
+              また、削除したアカウントは復元できません。
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>やめる</Button>
+            <Button
+              onClick={() => {
+                deleteMutation.mutate(clickedCreatorId);
+              }}
+              color="error"
+            >
+              削除する
+            </Button>
+          </DialogActions>
+        </Dialog>
       </Stack>
     </PageLayout>
   );
