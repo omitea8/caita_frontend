613ca79103e09820c50243e7c371df6ec7961a28
omitea8 <omitea8@gmail.com>
2023-06-30 18:06:44 +0900

    ページの内容を改修(仮)

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import { PageLayout } from "../components/PageLayout";
+import { Stack, Typography } from "@mui/material";
+import { Login } from "./Login";
 
 export const About: React.FC = () => {
   return (
     <PageLayout>
-      <p>caitaについて</p>
-      <p>AboutPage</p>
+      <Stack alignItems={"center"} spacing={3}>
+        <Typography variant="h2">caita</Typography>
+        <Typography variant="h6">
+          クリエイターのための作品管理プラットフォーム
+        </Typography>
+        <Stack alignItems={"center"} spacing={0}>
+          <Typography variant="body1">
+            Caitaとは、気軽に画像やイラストを投稿サービスです。
+          </Typography>
+          <Typography variant="body1">
+            シンプルに作品管理し、評価や反応を気にすることなく作品を投稿・公開することができます。
+          </Typography>
+        </Stack>
+        <Login />
+      </Stack>
     </PageLayout>
   );
 };
